02237e56e4f3f25030877075ecd11a9acbcaeb16
Emilio Bresolin <emiliotdm@hotmail.com>
2022-11-30 21:16:27 -0300

    add item view implementation

---
+++ app/src/main/java/com/example/suplistapp/view/AddItemActivity.java
@@ -0,0 +1,16 @@
+package com.example.suplistapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.suplistapp.R;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
